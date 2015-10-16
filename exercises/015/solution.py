2f2e98347db6ba5168c4b5bc6ccb0b760f2456d3
Klaklok <nikola.zareski@cri-paris.org>
2015-10-16 17:33:59 +0200

    Solution ?

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -1,7 +1,7 @@
 import operator
 a = operator.truediv(245850922, 78256779)
 b = operator.pow(5, 0.5)
-c = operator.truediv(b, 2)
-d = operator.add(1, c)
+c = operator.add(1, b)
+d = operator.truediv(c, 2)
 print(a)
 print(d)
