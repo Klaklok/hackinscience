274a15d60fd2d90682ed1e9d098a2b3278e62a57
Klaklok <nikola.zareski@cri-paris.org>
2015-10-16 17:30:24 +0200

    Test n°1

--- exercises/015/solution.py
+++ exercises/015/solution.py
@@ -1,3 +1,7 @@
 import operator
-print(operator.truediv(245850922, 78256779))
-print(1 + operator.pow(5, 0.5) / 2)
+a = operator.truediv(245850922, 78256779)
+b = operator.pow(5, 0.5)
+c = operator.truediv(b, 2)
+d = operator.add(1, c)
+print(a)
+print(d)
