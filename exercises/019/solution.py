f5564f310146f7b69457502eb402fdc62b3d09bc
Klaklok <nikola.zareski@cri-paris.org>
2015-11-06 14:32:38 +0100

    Test1

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,11 +1,9 @@
 import sys
 import operator
-if sys.argv >= 2:
-    try:
-        sys.argv[1] = int(sys.argv[1])
-        sys.argv[2] = int(sys.argv[2])
-        print(operator.add(sys.argv[1], sys.argv[2]))
-    except:
-        print("usage: python3 solution.py OP1 OP2")
+
+if len(sys.argv) < 2:
+    print("usage : python3 solution.py OP1 OP2")
 else:
-    print("usage: python3 solution.py OP1 OP2")
+    sys.argv[1] = int(sys.argv[1])
+    sys.argv[2] = int(sys.argv[2])
+    print(operator.add(sys.argv[1], sys.argv[2]))
