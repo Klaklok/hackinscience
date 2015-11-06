20cd88fe8caa95bff117997a681388a64ce088ce
Klaklok <nikola.zareski@cri-paris.org>
2015-11-06 14:34:52 +0100

    enlever un espace

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -2,7 +2,7 @@ import sys
 import operator
 
 if len(sys.argv) < 2:
-    print("usage : python3 solution.py OP1 OP2")
+    print("usage: python3 solution.py OP1 OP2")
 else:
     sys.argv[1] = int(sys.argv[1])
     sys.argv[2] = int(sys.argv[2])
