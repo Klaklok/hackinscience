97051ecd9e747dcda8abdd27860f09b705e3ee1c
Klaklok <nikola.zareski@cri-paris.org>
2015-11-13 15:44:18 +0100

    Test du jour

--- exercises/050/solution.py
+++ exercises/050/solution.py
@@ -1,4 +1,6 @@
 import operator
+s = 0
 for i in range(1, 1000):
-    if operator.mod(i, 3) == 0 or operator.mod(i, 5) == 0:
-        print(int(i))
+    if i % 3 == 0 or i % 5 == 0:
+        s += i
+print(s)
