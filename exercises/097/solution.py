ca88d157aedfa8dd94f4fa9bf62b76664a316a38
Klaklok <nikola.zareski@cri-paris.org>
2015-12-15 10:16:09 +0100

    Meat of love

---
+++ exercises/097/solution.py
@@ -0,0 +1,13 @@
+def love_meet(alice, bob):
+    i = set(alice)
+    j = set(bob)
+    return(i & j)        
+            
+
+def affair_meet(alice, bob, sylvester):
+    i = set(alice)
+    j = set(bob)
+    k = set(sylvester)
+    l = i & k
+    m = l-j
+    return(m)
