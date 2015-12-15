fb1a80d2985da78424027f6169b91169a90c45c5
Klaklok <nikola.zareski@cri-paris.org>
2015-12-15 09:00:22 +0100

    Repush avec écriture de la fonction sorted en entier

--- exercises/085/solution.py
+++ exercises/085/solution.py
@@ -1,6 +1,15 @@
 def sort_a_list(l):
-    return sorted(l, reverse=True)
-
+    num = len(l)-1
+    temp = 0
+    while num > 0:
+        for i in range(num):
+            if l[i] > l[i+1]:
+                temp = l[i]
+                l[i] = l[i+1]
+                l[i+1] = temp
+        num -= 1
+    return(l)
+        
 
 def sort_by_mark(my_class):
     return sorted(my_class, reverse=True)
